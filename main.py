58b5a82474787deb226fccb86f5ce2551a53cd90
Евгений <evgen19-97@ya.ru>
2019-11-14 18:02:40 +0300

    add

--- main.py
+++ main.py
@@ -4,9 +4,12 @@ parser = argparse.ArgumentParser(
     description='Process parametrs to run simulation')
 
 parser.add_argument('-rs', '--random-seed', type=int, default=42)
-parser.add_argument('-sz', '--sample-size', type=int, required=True)
+parser.add_argument('-sz', '--sample-size', type=int, default=2)
 parser.add_argument('-rr', '--recombination-rate', type=float, required=True)
 parser.add_argument('-mr', '--mutation-rate', type=float, required=True)
-parser.add_argument('-m', '--model', type=str, required=True)
+parser.add_argument('-m', '--model', type=str, default='hudson')
+parser.add_argument('-de', '--demographic_events', type=list, required=True)
+parser.add_argument('-nr', '--num_replicates', type=int, required=True)
+
 
 args = parser.parse_args()
