e3e8cf0e45541557ca5969ed90063df33e615826
Евгений <evgen19-97@ya.ru>
2019-11-15 18:08:52 +0300

    add run to init

--- utilities/__init__.py
+++ utilities/__init__.py
@@ -1,4 +1,4 @@
-from .generator import *
+import msprime
 
 
 class DataGenerator():
@@ -6,26 +6,45 @@ class DataGenerator():
     """
 
     def __init__(self, sample_size: int, recombination_rate: float,
-                 mutation_rate: float, model: str, random_seed: int = 42):
+                 mutation_rate: float, model: str, demographic_events: list,
+                 num_replicates: int, random_seed: int):
         self.sample_size = sample_size
         self.recombination_rate = recombination_rate
         self.mutation_rate = mutation_rate
+        self.num_replicates = num_replicates
+        self.demographic_events = demographic_events
         self.model = model
         self.random_seed = random_seed
 
     def __str__(self):
         return """DataGenerator class:\n sample_size = {}\n
          recombination_rate = {}\n mutation_rate = {}\n
-         model = {}\n random_seed={}""".format(
+         model = {}\n num_replicates = {}\n
+          demographic_events = {}\n
+          random_seed = {}\n""".format(
             self.sample_size,
             self.recombination_rate,
             self.mutation_rate,
             self.model,
+            self.num_replicates,
+            self.demographic_events,
             self.random_seed
         )
 
-    def __call__(self):
-        pass
+    def run_simulation(self):
+        """
+        return generator(tskit.TreeSequence)
+        function run the simulation with given parametrs
+        """
+        return msprime.simulate(
+            sample_size=self.sample_size,
+            recombination_rate=self.recombination_rate,
+            mutation_rate=self.mutation_rate,
+            random_seed=self.random_seed,
+            model=self.model,
+            num_replicates=self.num_replicates,
+            demographic_events=self.demographic_events
+        )
 
-    def generate(self):
+    def __call__(self):
         pass
