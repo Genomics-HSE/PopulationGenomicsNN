e55c81818939cfdcc6a0a7f77fba192c2f664263
Евгений <evgen19-97@ya.ru>
2020-01-15 10:56:12 +0300

    rewrite; add to do;

--- neuralNetwork.py
+++ neuralNetwork.py
@@ -8,6 +8,18 @@ import torch
 import utilities
 
 
+# TODO
+# 1) Splite main to functions;
+# 2) Separete consts;
+# 3) Use arguments;
+# 4) Add db to store results;
+# 5) Separate NN class, rename in main;
+# 6) Separate LOSS;
+
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+
 class MyDataset(torch.utils.data.Dataset):
     def __init__(self, x, y):
         super(MyDataset, self).__init__()
@@ -66,55 +78,52 @@ def make_train_step(model, loss_fn, optimizer):
     return train_step
 
 
-"""Create data generator"""
-
-events = utilities.generate_demographic_events()
-
-Ne = 1
-rho = 1.6*10e-9*Ne
-mu = 1.25*10e-8*Ne
-num_repl = int(1e5)  # 5
-l = int(3e3)
-
-dg = utilities.DataGenerator(recombination_rate=rho,
-                             mutation_rate=mu,
-                             demographic_events=events,
-                             num_replicates=num_repl, lengt=l)
-data = dg.run_simulation()
-
-"""Create datasets"""
-
-number_train_examples = int(num_repl*.9)
-
-trX, trY = [], []
-for i in range(number_train_examples):
-    example = next(dg)
-    trX.append(example[0])
-    trY.append(example[1])
-
-teX, teY = [], []
-for example in dg:
-    teX.append(example[0])
-    teY.append(example[1])
-
-
-del dg
-
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-
-input = torch.from_numpy(np.array(trX, dtype=np.float_))  # .to(device)
-target = torch.from_numpy(np.array(trY))  # .to(device)
-test_input = torch.from_numpy(np.array(teX, dtype=np.float_))  # .to(device)
-test_target = torch.from_numpy(np.array(teY))  # .to(device)
-
-del trX, trY, teX, teY
-
-traindata = MyDataset(input, target)
-
-loss_per_step = []
-
-
 def main():
+    """Create data generator"""
+
+    events = utilities.generate_demographic_events()
+
+    Ne = 1
+    rho = 1.6*10e-9*Ne
+    mu = 1.25*10e-8*Ne
+    num_repl = int(1e5)  # 5
+    l = int(3e3)
+
+    dg = utilities.DataGenerator(recombination_rate=rho,
+                                 mutation_rate=mu,
+                                 demographic_events=events,
+                                 num_replicates=num_repl, lengt=l)
+    dg.run_simulation()
+
+    """Create datasets"""
+
+    number_train_examples = int(num_repl*.9)
+
+    trX, trY = [], []
+    for i in range(number_train_examples):
+        example = next(dg)
+        trX.append(example[0])
+        trY.append(example[1])
+
+    teX, teY = [], []
+    for example in dg:
+        teX.append(example[0])
+        teY.append(example[1])
+
+    del dg
+
+    input = torch.from_numpy(np.array(trX, dtype=np.float_))  # .to(device)
+    target = torch.from_numpy(np.array(trY))  # .to(device)
+    test_input = torch.from_numpy(
+        np.array(teX, dtype=np.float_))  # .to(device)
+    test_target = torch.from_numpy(np.array(teY))  # .to(device)
+
+    del trX, trY, teX, teY
+
+    traindata = MyDataset(input, target)
+
+    loss_per_step = []
+
     # set random seed to 0
     np.random.seed(0)
     torch.manual_seed(0)
@@ -145,6 +154,7 @@ def main():
                 print(f'BATCH: {i+1}/{total_step} LOSS: {loss}')
         loss_per_step.append(total_loss)
 
+    print(loss_per_step)
+
 
 main()
-loss_per_step
