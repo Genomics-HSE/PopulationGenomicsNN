efdeb8d26170cdb0dd41d140c3afdfd6bfe51d7e
Евгений <evgen19-97@ya.ru>
2020-01-29 14:20:18 +0300

    works

--- main.py
+++ main.py
@@ -8,56 +8,53 @@ import numpy as np
 
 import utilities
 
-# Шаг первый: извлекли аргементы - параметры запуска.
 
 parser = argparse.ArgumentParser()
-parser.add_argument('Ne', type=float, default=1.0, help='Ask VL')
-parser.add_argument('rho', type=float, default=1.6*10e-9, help='')
-parser.add_argument('mu', type=float, default=1.25*10e-8, help='')
-parser.add_argument('num_repl', type=int, default=int(1e5), help='')
-parser.add_argument('l', type=int, default=int(3e3), help='')
-parser.add_argument('ratio_train_examples', type=float, default=0.9, help='')
-parser.add_argument('device', type=str, default='cuda' if torch.cuda.is_available()
+parser.add_argument('-Ne', type=float, default=1.0, help='Ask VL')
+parser.add_argument('-rho', type=float, default=1.6*10e-9, help='')
+parser.add_argument('-mu', type=float, default=1.25*10e-8, help='')
+parser.add_argument('-num_repl', type=int, default=int(1e5), help='')
+parser.add_argument('-l', type=int, default=int(3e3), help='')
+parser.add_argument('-ratio_train_examples', type=float, default=0.9, help='')
+parser.add_argument('-device', type=str, default='cuda' if torch.cuda.is_available()
                     else 'cpu', help='Specify use cpu/cuda')
-parser.add_argument('result_dir', default='results/',
+parser.add_argument('-result_dir', default='results/',
                     help='')
-parser.add_argument('random_seed', default=np.random.randint(
+parser.add_argument('-random_seed', default=np.random.randint(
     10000000000), help='')
-parser.add_argument('steps', type=int, default=20)
+parser.add_argument('-steps', type=int, default=0)
 parser.add_argument('model')
 
 args = parser.parse_args()
 
-# Шаг второй: импортируем модуль с моделью
-NN = importlib.import_module(args.model)
-
+#NN = importlib.import_module(args.model)
+import models.simplest as NN
 
 np.random.seed(args.random_seed)
 torch.manual_seed(args.random_seed)
-# Шаг третий: создать генератор
 
 traindata, test_data = utilities.make_dataset(args)
 train_loader = torch.utils.data.DataLoader(traindata, batch_size=64,
                                            shuffle=True)
 
-test_loader = torch.utils.data.DataLoader()
-# Шаг четвертый: создаем модель
+test_loader = torch.utils.data.DataLoader(test_data)
+
 model = NN.model()
 model = model.to(args.device)
 model.double()  # Why?
 
-# Шаг пятый: определяем LOSS
+
 criterion = NN.LOSS()
 criterion = criterion.to(args.device)
 
-# Шаг шестой: определяем оптимизатор
+
 optimizer = NN.optimizer(model)
 
 
 train_step = NN.make_train_step(model, criterion, optimizer)
 total_step = len(train_loader)
 
-# Шаг седьмой: обучаем
+
 loss_per_step = []
 
 for i in range(args.steps):
@@ -67,14 +64,15 @@ for i in range(args.steps):
         total_loss += loss
     loss_per_step.append(total_loss)
 
-# Шаг восьмой: считаем итоговый результат
-results = 0.0
-with torch.no_grad():
-    for data in test_loader:
-        input, target = data
-        results += abs(model(input) - target)
 
-# Шаг девятый: сохраняем все результаты
+results = 0.0
+if False:
+    with torch.no_grad():
+        for data in test_loader:
+            input, target = data
+            results += abs(model(input) - target)
+
+
 now = datetime.today().strftime('%Y-%m-%d-%H:%M:%S')
 output_info = {
     'date': now,
@@ -84,7 +82,7 @@ output_info = {
     'parametrs':
     {
         'random_seed': args.random_seed,
-        'train_steps': args.train_steps,
+        'train_steps': args.steps,
         'device': args.device,
         'events': None,
         'Ne': args.Ne,
@@ -92,10 +90,10 @@ output_info = {
         'mu': args.mu,
         'num_repl': args.num_repl,
         'l': args.l,
-        'ration_train_examples': args.ration_train_examples
+        'ration_train_examples': args.ratio_train_examples
     }
 }
 
-filename = args.result_dir + f'{now}.json'
-with open(filename, 'w') as outfile:
+filename = args.result_dir + str(now) + '.json'
+with open(filename, 'w+') as outfile:
     json.dump(output_info, outfile)
--- utilities/__init__.py
+++ utilities/__init__.py
@@ -1,4 +1,5 @@
 import numpy as np
+import torch
 
 import msprime
 from math import (exp, log)
